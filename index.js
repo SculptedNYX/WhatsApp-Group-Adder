60db2e08643048900cb25b5a5f5f5f934316dcd8
Nyx <DarkValvet@protonmail.com>
2023-10-30 17:01:18 +0200

    added google contacts sync prompt

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const prompt = require('prompt-sync')();
 const google_contacts = require('./google_contacts');
 
 (async () => {
-    const choice = prompt("Do you want to create and import a CSV? (required if numbers arent your contacts)... (Yes/No/Exit) ").toLowerCase();
+    let choice = '';
+    choice = prompt("Do you want to create and import a CSV? (required if numbers arent your contacts)... (Yes/No/Exit) ").toLowerCase();
     if(choice == 'yes'){
         const email =  prompt("Enter you google contacts Gmail: ");
         const password =  prompt("Enter you google contacts password: ");
@@ -13,7 +14,9 @@ const google_contacts = require('./google_contacts');
         exit();
     }
 
-    console.log("Generating whatsapp web QR code");
-
-    await require('./whatsapp_adder');
+    choice = prompt("Are the files synced on your google contacts app? (Yes/yes)").toLowerCase();
+    if(choice == 'yes'){
+        console.log("Generating whatsapp web QR code");
+        await require('./whatsapp_adder');
+    }
 })();
