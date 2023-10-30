225c6c899fa90c69033ce3ccc67a1ebd70a4e095
Nyx <DarkValvet@protonmail.com>
2023-10-30 17:59:04 +0200

    fixed async issues

--- csv_creator.js
+++ csv_creator.js
@@ -1,30 +1,36 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const exp = require('constants');
 const fs = require('fs');
 
-const FILENAME = 'numbers.txt';
 
-// Creates CSV file with appropriate google import headers
-const csvWriter = createCsvWriter({
-    path: 'contacts.csv',
-    header: [
-        { id: 'Name', title: 'Name' },
-        { id: 'Family', title: 'Family Name' },
-        { id: 'Type', title: 'Phone 1 - Type' },
-        { id: 'Phone', title: 'Phone 1 - Value' }
-    ]
-});
 
-let records = [];
-const numberList = fs.readFileSync(FILENAME).toString().split("\n");
-const numberListLength = numberList.length;
-// Adds all the numbers to the records array
-for(let i = 0; i < numberListLength; i ++){
-    records.push({Name: `${(i+1)}`,Family: `${(i+1)}` ,Type: '', Phone: numberList[i]});
-}
+async function csv_creator_fn(){
+    const FILENAME = 'numbers.txt';
 
-// Writes to the CSV
-console.log("Creating CSV");
-csvWriter.writeRecords(records)       // returns a promise
-    .then(() => {
-        console.log('...Done');
+    // Creates CSV file with appropriate google import headers
+    const csvWriter = createCsvWriter({
+        path: 'contacts.csv',
+        header: [
+            { id: 'Name', title: 'Name' },
+            { id: 'Family', title: 'Family Name' },
+            { id: 'Type', title: 'Phone 1 - Type' },
+            { id: 'Phone', title: 'Phone 1 - Value' }
+        ]
     });
+    
+    let records = [];
+    const numberList = fs.readFileSync(FILENAME).toString().split("\n");
+    const numberListLength = numberList.length;
+    // Adds all the numbers to the records array
+    for(let i = 0; i < numberListLength; i ++){
+        records.push({Name: `${(i+1)}`,Family: `${(i+1)}` ,Type: '', Phone: numberList[i]});
+    }
+    
+    // Writes to the CSV
+    console.log("Creating CSV");
+    await csvWriter.writeRecords(records)       // returns a promise
+        .then(() => {
+            console.log('...Done');
+        });
+}
+module.exports = {csv_creator_fn};
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const prompt = require('prompt-sync')();
-
+const csv = require('./csv_creator');
 (async () => {
     let choice = '';
-    choice = prompt("Do you want to create a CSV for your google contacts? (required if numbers arent your contacts)... (Yes/No/Exit) ").toLowerCase();
+    choice = prompt("Do you want to create and import a CSV? (required if numbers arent your contacts)... (Yes/No/Exit) ").toLowerCase();
     if(choice == 'yes'){
-        await require('./csv_creator');
-        await console.log("Please import the CSV file to google contacts");
+        await csv.csv_creator_fn();
     }
     else if (choice == 'Exit'){
         exit();
     }
-    await console.log("Please make sure you are an admin in the group other wise all the numbers will be dumped in the dublicate numbers list");
+
     choice = prompt("Are the files synced on your google contacts app? (Yes/yes)").toLowerCase();
     if(choice == 'yes'){
         console.log("Generating whatsapp web QR code");
-        await require('./whatsapp_adder');
+        require('./whatsapp_adder');
     }
 })();
